9e4b6ccf8511616f26ff559736aa983fd8cf3913
你çYedada <1327494635@qq.com>
2026-04-19 22:12:32 +0800

    update deepar script

---
+++ scripts/ETT_script/DeepAR_ETTm1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+model_name=DeepAR
+seq_len=168
+d_model=128
+
+for pred_len in 48 96 192 336
+# for pred_len in 96 192 336 720
+do
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_168_$pred_len \
+  --model $model_name \
+  --data ETTm1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len $pred_len \
+  --e_layers 2 \
+  --d_model $d_model \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --des 'Exp' \
+  --loss_type quantileLoss \
+  --quantiles "[0.1, 0.5, 0.9]" \
+  --itr 1 \
+  --batch_size 32 \
+  --step 1 \
+  --learning_rate 0.001
+
+done
