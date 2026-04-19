008b51b60aab67eadb19597b3d442ddfa3ba63ad
Yedada <1327494635@qq.com>
2026-04-19 21:59:15 +0800

    update scripts

--- scripts/ECL_script/ECL_ALL.sh
+++ scripts/ECL_script/ECL_ALL.sh
@@ -1,12 +1,12 @@
-bash ./scripts/long_term_forecast/ECL_script/Autoformer.sh
-bash ./scripts/long_term_forecast/ECL_script/DeepAR.sh
-bash ./scripts/long_term_forecast/ECL_script/DLinear.sh
-bash ./scripts/long_term_forecast/ECL_script/iTransformer.sh
-bash ./scripts/long_term_forecast/ECL_script/PatchTST.sh
-bash ./scripts/long_term_forecast/ECL_script/SPECTRA.sh
-bash ./scripts/long_term_forecast/ECL_script/TADNet.sh
-bash ./scripts/long_term_forecast/ECL_script/TemporalFusionTransformer.sh
-bash ./scripts/long_term_forecast/ECL_script/TiDE.sh
-bash ./scripts/long_term_forecast/ECL_script/TimesNet.sh
-bash ./scripts/long_term_forecast/ECL_script/TimeXer.sh
+bash ./scripts/ECL_script/Autoformer.sh
+bash ./scripts/ECL_script/DeepAR.sh
+bash ./scripts/ECL_script/DLinear.sh
+bash ./scripts/ECL_script/iTransformer.sh
+bash ./scripts/ECL_script/PatchTST.sh
+bash ./scripts/ECL_script/SPECTRA.sh
+bash ./scripts/ECL_script/TADNet.sh
+bash ./scripts/ECL_script/TemporalFusionTransformer.sh
+bash ./scripts/ECL_script/TiDE.sh
+bash ./scripts/ECL_script/TimesNet.sh
+bash ./scripts/ECL_script/TimeXer.sh
 
